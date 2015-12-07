4f3a305de2b92dbaebc3554a0e73968bc5e010ea
Eric Satterwhite <esatterwhite@wi.rr.com>
2015-12-07 10:36:48 -0600

    adding boilerplate for links and volumes
    Line to create a redis client
    line to grab some configurations from a json file

--- 02-containers/02.3-linking-containers/index.js
+++ 02-containers/02.3-linking-containers/index.js
@@ -1,26 +1,30 @@
-var express = require( 'express' )
+var fs = require('fs')
+  , express = require( 'express' )
   , redis = require( 'redis' )
   , app = express()
   , client
+  , config
   ;
 
-client = redis.createClient( /*port*/, /*host*/ );
+client = redis.createClient( /*port*/ /*, host*/ );
+// config = JSON.parse( fs.readfileSync() )
 
 app.post('/incr', function( req, res ){
 	client.incr('docker:count', function(err, count){
-        res.status(201).send(''+count);
-    })
+        res.status(201).send(count);
+    });
 });
 
 app.post('/decr', function( req, res ){
     client.decr('docker:count', function( err, count ){
-        if( count <= 0 || err){
+        if( count <= 0 || err ){
+            cli.del('docker:count');
             return res.status(400).send('count already at lowest value');
         }
-        res.status(201).send('' + ( --count ));
-    })
+        res.status(201).send(count);
+    });
 });
 
-app.listen( 3000 ,function( err ){
+app.listen( 3000, function( err ){
 	console.log( 'app running: %s',  'https://0.0.0.0:3000');
 });
