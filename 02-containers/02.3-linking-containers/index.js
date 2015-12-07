cec16258e823e2b00168b0031c9e9abefc4f8835
Eric Satterwhite <esatterwhite@wi.rr.com>
2015-12-07 13:16:37 -0600

    Lesson 02.3
    Read from redis host name and a mounted directory

--- 02-containers/02.3-linking-containers/index.js
+++ 02-containers/02.3-linking-containers/index.js
@@ -4,24 +4,29 @@ var fs = require('fs')
   , app = express()
   , client
   , config
+  , LOWER_BOUND
   ;
 
-client = redis.createClient( /*port*/ /*, host*/ );
-// config = JSON.parse( fs.readfileSync() )
+client = redis.createClient(6379, 'redis');
+config = require('/etc/config/app.json');
+LOWER_BOUND = config.lower_bound || 0;
+
+console.log('LOWER_BOUND set to %s', LOWER_BOUND)
 
 app.post('/incr', function( req, res ){
 	client.incr('docker:count', function(err, count){
-        res.status(201).send(count);
+        res.status(201).send(""+count);
     });
 });
 
 app.post('/decr', function( req, res ){
     client.decr('docker:count', function( err, count ){
-        if( count <= 0 || err ){
-            cli.del('docker:count');
+        if( count <= LOWER_BOUND || err ){
+            err && console.log( err )
+            client.del('docker:count');
             return res.status(400).send('count already at lowest value');
         }
-        res.status(201).send(count);
+        res.status(201).send('' + count);
     });
 });
 
